30ab65014d1ce0e0c8dc4ea077e7848e76264867
yun <1154398717@qq.com>
2022-10-31 20:53:37 +0800

    每一个model 的admin搭建  sidebar字段的修改

--- typeidea/typeidea/config/admin.py
+++ typeidea/typeidea/config/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Link, SideBar
+
+# Register your models here.typeidea/typeidea/config/models.py
+
+@admin.register(Link)
+class LinkAdmin(admin.ModelAdmin):
+    list_display = ('title', 'href', 'status', 'weight', 'created_time')
+    fields = ('title', 'href', 'status', 'weight')
+
+    def save_model(self, request, obj, change):
+        obj.user = request.user
+        return super(LinkAdmin, self).save_model(self, request, obj, change)
+
+
+@admin.register(SideBar)
+class SideBarAdmin(admin.ModelAdmin):
+    list_display = ('title', 'display_type', 'content','created_time')
+    fields = ('titie', 'display_type', 'content')
+
+    def save_model(self, request, obj, change):
+       obj.user = request.user
+       return super(SideBarAdmin, self).save_model(self, request, obj, change) 
